5e9ae138853a61c360d3b5561d957ce034b4c325
changjoon-park <sisyphus9402@gmail.com>
2024-02-04 16:12:23 +0000

    Study #1-2 - Enhance embed_vector.py: show present features

--- codes/embed_vector.py
+++ codes/embed_vector.py
@@ -19,7 +19,7 @@ applications = {
 
 # Define the indicator function I(x, s)
 def indicator_function(app_features, feature):
-    return 1 if feature in app_features else 0
+    return (1, feature) if feature in app_features else (0, feature)
 
 # Implement the embedding function phi(x) to create binary vector representations
 def embed_app_in_vector_space(app_features, all_possible_features):
@@ -30,6 +30,17 @@ def embed_app_in_vector_space(app_features, all_possible_features):
 embedded_applications = {app_name: embed_app_in_vector_space(features, all_features)
                          for app_name, features in applications.items()}
 
-embedded_applications
+print("[All features]")
+for feature in all_features:
+    print(f"  - {feature}")
 
 
+print("\n[App: Vector - Feature(present)]")
+for app, indicator in embedded_applications.items():
+    present = []
+    vector = []
+    for element, feature in indicator:
+        vector.append(element)
+        if element == 1:
+            present.append(feature)
+    print(f"{app}: {vector} - {present}")
