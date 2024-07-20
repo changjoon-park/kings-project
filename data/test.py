f2a398a35258e6096e399563e38460a53cd8075b
Changjoon <sisyphus9402@gmail.com>
2024-07-20 17:04:54 +0100

    Experiment 1: Completed

--- data/test.py
+++ data/test.py
@@ -1,51 +1,79 @@
-## Parameters: XGBoost
-xgb_parameters = {
-    "n_estimators": 200,
-    "learning_rate": 0.05,
-    "max_depth": 4,
-    "min_child_weight": 3,
-    "subsample": 0.9,
-    "colsample_bytree": 0.9,
-}
+from dataclasses import dataclass, field
+from sklearn.metrics import confusion_matrix
+from typing import Any
 
-## Parameters: Linear SVM
-svm_parameters = {
-    "max_iter": 50000,
-}
 
-## Update specific models with desired parameters
-updated_models = []
-for model in models:
-    if model.name == "XGBoost":
-        updated_model = Model(model.name, xgboost(**xgb_parameters))
-    elif model.name == "Linear SVM":
-        updated_model = Model(
-            model.name, linear_support_vector_machine(**svm_parameters)
+@dataclass
+class Model:
+    name: str
+    model: Any
+    accuracy: float = field(init=False, default=None)
+    precision: float = field(init=False, default=None)
+    recall: float = field(init=False, default=None)
+    f1: float = field(init=False, default=None)
+    cm: np.ndarray = field(init=False, default=None)
+    roc_auc: float = field(init=False, default=None)
+    fpt: float = field(init=False, default=None)
+    tpr: float = field(init=False, default=None)
+    precision_curve: float = field(init=False, default=None)
+    recall_curve: float = field(init=False, default=None)
+    average_precision: float = field(init=False, default=None)
+
+    def __str__(self):
+        return self.name
+
+    def train_and_evaluate_model(self, X_train, y_train, X_test, y_test):
+        self.model.fit(X_train, y_train)
+
+        predictions = self.model.predict(X_test)
+
+        self.accuracy, self.precision, self.recall, self.f1 = calculate_metrics(
+            y_test, predictions
         )
-    else:
-        updated_model = model
-    updated_models.append(updated_model)
+        self.cm = confusion_matrix(y_test, predictions)
 
-for model in updated_models:
-    model.train_model(X_train, y_train)
-    (
-        model,
-        accuracy,
-        precision,
-        recall,
-        f1,
-        cm,
-        roc_auc,
-        fpr,
-        tpr,
-        precision_curve,
-        recall_curve,
-        average_precision,
-    ) = model.evaluate_model(X_test, y_test)
+        y_scores = self.get_prediction_scores(X_test)
+        (
+            self.fpr,
+            self.tpr,
+            self.roc_auc,
+            self.precision_curve,
+            self.recall_curve,
+            self.average_precision,
+        ) = calculate_roc_pr(y_test, y_scores)
 
-    print_metrics(
-        model, accuracy, precision, recall, f1, cm, roc_auc, average_precision
-    )
-    plot_results(
-        model, fpr, tpr, roc_auc, precision_curve, recall_curve, average_precision, cm
-    )
+    def get_prediction_scores(self, X_test):
+        if hasattr(self.model, "predict_proba"):
+            return self.model.predict_proba(X_test)[:, 1]
+        elif hasattr(self.model, "decision_function"):
+            return self.model.decision_function(X_test)
+        else:
+            raise AttributeError(
+                "Model has neither predict_proba nor decision_function method"
+            )
+
+    def print_result(self):
+        print_data_info(X_train, X_test, y_train, y_test, self.name)
+        print_metrics(
+            self.name,
+            self.accuracy,
+            self.precision,
+            self.recall,
+            self.f1,
+            self.cm,
+            self.roc_auc,
+            self.average_precision,
+        )
+        plot_results(
+            self.name,
+            self.fpr,
+            self.tpr,
+            self.roc_auc,
+            self.precision_curve,
+            self.recall_curve,
+            self.average_precision,
+            self.cm,
+            coef=None,
+            importances=None,
+            feature_names=None,
+        )
